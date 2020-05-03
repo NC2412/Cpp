c5a02d2366127e6c42426cff01cbee4bf196cbc0
Nicolo Cammarano <nic_camm@icloud.com>
2020-05-03 14:09:53 -0700

    Templatized Queue class, fixed bug thanks to Mr. Magruder! No more Queue.cpp

--- Projects/My Queue/My Queue/My Queue.cpp
+++ Projects/My Queue/My Queue/My Queue.cpp
@@ -7,12 +7,12 @@ int main()
 {
     Queue <int>queue;
 
-    int addArr[] = { 1, 2 };
+    int addArr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
     for (auto val : addArr) {
         queue.enqueue(val);
     }
 
-    for (auto i : addArr) {
+    while (!queue.isEmpty()) {
         cout << queue.dequeue() << ", " << flush;
     }
 }

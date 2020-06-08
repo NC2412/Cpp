b30f83a8f5404577de4cdabe9b440e9cb58c739a
NC2412 <nic_camm@icloud.com>
2020-06-08 12:28:04 -0700

    Some more work on threading a functor. Two errors show up from the thread header file.

--- Practice/Threading and Locking/Threading and Locking/Threading and Locking.cpp
+++ Practice/Threading and Locking/Threading and Locking/Threading and Locking.cpp
@@ -5,46 +5,42 @@ using namespace std;
 
 mutex mtx;
 
-void print(const string text) {
+void print(const char text[]) {
 	mtx.lock();
-	cout << text << endl;
+	for (int i = 0; i < sizeof(text); i++) {
+		cout << text[i] << flush;
+	}
+	cout << endl;
 	mtx.unlock();
 }
 
 // Keep count unsigned and const so it cannot change and it is never less that 0 (so the for loop doesn't break).
 void threadFunc1(const unsigned int count) {
 	for (unsigned int i = 0; i < count; i++) {
-		print((char)i + ": Hello from thread function 1!");
+		print(i + ": Hello from thread function 1!");
 	}
 }
 
 void threadFunc2(const unsigned int count) {
 	for (unsigned int i = 0; i < count; i++) {
-		print((char)i + ": Hello from thread function 2!");
+		print(i + ": Hello from thread function 2!");
 	}
 }
 
-class personFunctor {
-public:
-	personFunctor(const int age, const string name) : age(age), name(name) {}
-
-	string operator()() {
-		return "Name: " + name + ", Age: " + (char)age + ", ";
+class Functor {
+	void operator()(const char text[]) {
+		print(text);
 	}
-
-private:
-	int age;
-	string name;
 };
-
 
 int main() {
 
-	personFunctor pfunc(17, "Nicolo Cammarano");
+	Functor functor();
+	//functor("Hello from () overload from main. NOT A THREAD.");
 
 	thread t1(threadFunc1, 10);
 	thread t2(threadFunc2, 10);
-	//thread t3(pfunc());
+	thread t3(functor, "Hello from functor thread!");
 	thread t4([](const unsigned int count) {
 		for (unsigned int i = 0; i < count; i++) {
 			print((char)i + ": Hello from lambda thread!");
@@ -53,7 +49,7 @@ int main() {
 	
 	t1.join();
 	t2.join();
-	//t3.join();
+	t3.join();
 	t4.join();
 
 	return 0;

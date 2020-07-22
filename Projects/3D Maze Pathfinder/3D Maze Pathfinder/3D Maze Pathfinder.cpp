8c634eec9c82a7f78f983677c37cdecb822e5983
NC2412 <nic_camm@icloud.com>
2020-07-21 22:57:58 -0700

    Pathfinder now complete, does not return shortest possible path... Bug in recursion order?

--- Projects/3D Maze Pathfinder/3D Maze Pathfinder/3D Maze Pathfinder.cpp
+++ Projects/3D Maze Pathfinder/3D Maze Pathfinder/3D Maze Pathfinder.cpp
@@ -1,50 +1,63 @@
 #include <iostream>
 using namespace std;
 
-// SOLUTION IS: R, I, D, D, I, R, U, R, I , D, D, O
-const int maze[4][4][4] = { { {0,-1,-1,-1}, {0,0,-1,-1}, {0,0,-1,-1}, {-1,-1,-1,0} },
+// SOLUTION IS: R, I, D, D, I, R, U, R, I, D, D, O
+//
+// KEY: 0 = OPEN
+//      -1 = BLOCKED
+//      1 = VISITED
+//
+int maze[4][4][4] = { { {0,-1,-1,-1}, {0,0,-1,-1}, {0,0,-1,-1}, {-1,-1,-1,0} },
                             { {0,-1,-1,-1}, {-1,0,-1,-1}, {-1,-1,0,-1}, {-1,-1,0,0} },
                             { {-1,-1,0,-1}, {-1,0,0,-1}, {-1,-1,0,-1}, {-1,-1,-1,0} },
                             { {-1,0,0,-1}, {-1,0,-1,-1}, {-1,-1,-1,-1}, {-1,-1,-1,0} }, };
 
-
-
 string findPath(int x, int y, int z) {
+    string result = "";
+    maze[x][y][z] = 1;
+
     // Check if we are at the end of the maze
-    if (x ==3 && y == 3 && z == 3)
-        return "O!";
+    if (x == 3 && y == 3 && z == 3) {
+        cout << "X: " << x << ", Y: " << y << ", Z: " << z << endl;
+        return "FINISHED!";
+    }
 
     // Check if up is 0
-    if (maze[x - 1][y][z] == 0 && x - 1 > -1) {
-        string result = findPath(x - 1, y, z);
-        return result == "" ? result : "U, " + result;
+    if (x - 1 > -1 && maze[x - 1][y][z] == 0) {
+        cout << "X: " << x << ", Y: " << y << ", Z: " << z << endl;
+        string temp = findPath(x - 1, y, z);
+        result = temp == "" ? result : "U, " + temp;
     }
 
     // Check if left is 0
-    if (maze[x][y - 1][z] == 0 && y - 1 > -1) {
-        string result = findPath(x, y - 1, z);
-        return result == "" ? result : "L, " + result;
+    if (y - 1 > -1 && maze[x][y - 1][z] == 0) {
+        cout << "X: " << x << ", Y: " << y << ", Z: " << z << endl;
+        string temp = findPath(x, y - 1, z);
+        result = temp == "" ? result : "L, " + temp;
     }
 
     // Check if down is 0
-    if (maze[x + 1][y][z] == 0 && (x + 1 < sizeof(maze) / sizeof(maze[0]))) {
-        string result = findPath(x + 1, y, z);
-        return result == "" ? result : "D, " + result;
+    if ((x + 1 < sizeof(maze) / sizeof(maze[0])) && maze[x + 1][y][z] == 0) {
+        cout << "X: " << x << ", Y: " << y << ", Z: " << z << endl;
+        string temp = findPath(x + 1, y, z);
+        result = temp == "" ? result : "D, " + temp;
     }
 
     // Check if right is 0
-    if (maze[x][y + 1][z] == 0 && (y + 1 < sizeof(maze[0]) / sizeof(maze[0][0]))) {
-        string result = findPath(x, y + 1, z);
-        return result == "" ? result : "R, " + result;
+    if ((y + 1 < sizeof(maze[0]) / sizeof(maze[0][0])) && maze[x][y + 1][z] == 0) {
+        cout << "X: " << x << ", Y: " << y << ", Z: " << z << endl;
+        string temp = findPath(x, y + 1, z);
+        result = temp == "" ? result : "R, " + temp;
     }
 
     // Check if in is 0
-    if (maze[x][y][z + 1] == 0 && (z + 1 < sizeof(maze[0][0]) / sizeof(int))) {
-        string result = findPath(x, y, z + 1);
-        return result == "" ? result : "I, " + result;
+    if ((z + 1 < sizeof(maze[0][0]) / sizeof(int)) && maze[x][y][z + 1] == 0) {
+        cout << "X: " << x << ", Y: " << y << ", Z: " << z << endl;
+        string temp = findPath(x, y, z + 1);
+        result = temp == "" ? result : "I, " + temp;
     }
 
-    return "";
+    return result;
 }
 
 int main()
